9fe5a5e56e805124cb724d2959279fdb52c33024
Bob Gilmore <dev@bobgilmore.name>
2014-02-11 03:31:20 +0000

    Write location to global git var for reuse on first use

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 # Find the location of the script.
 DOTFILE_DIRECTORY=`pwd`
 
-echo "Creating symbolic links in .git/hooks..."
+echo "Creating and registering symbolic links in .git/hooks..."
 rm -rf "$1/.git/hooks"
 ln -is "$DOTFILE_DIRECTORY" "$1/.git/hooks"
+git config --global hooks.symlinksourcerepo "$DOTFILE_DIRECTORY"
 
 echo "...done"
