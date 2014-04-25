c2a19ea68b7ba608ba7af6ad584468fa337b4aef
Bob Gilmore <dev@bobgilmore.name>
2014-04-25 01:44:42 -0400

    Make setup script robust to being run from any location

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Find the location of the script.
-DOTFILE_DIRECTORY=`pwd`
+HOOKS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Creating and registering symbolic links in .git/hooks..."
 rm -rf "$1/.git/hooks"
-ln -is "$DOTFILE_DIRECTORY" "$1/.git/hooks"
-git config --global hooks.symlinksourcerepo "$DOTFILE_DIRECTORY"
+ln -is "$HOOKS_DIRECTORY" "$1/.git/hooks"
+git config --global hooks.symlinksourcerepo "$HOOKS_DIRECTORY"
 
 echo "...done"
