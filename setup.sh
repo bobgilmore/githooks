b5d9a5185b9a2c4004272c122b573e5ad2be8fb6
Bob Gilmore <dev@bobgilmore.name>
2014-05-06 20:45:00 -0400

    Backup old hooks directory before overwriting

--- setup.sh
+++ setup.sh
@@ -4,7 +4,13 @@
 HOOKS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Creating and registering symbolic links in .git/hooks..."
-rm -rf "$1/.git/hooks"
+if [[ -d "$1/.git/hooks" || -L "$1/.git/hooks" ]]; then
+	if [[ -d "$1/.git/hooksOLD" || -L "$1/.git/hooksOLD" ]]; then
+		rm -rf "$1/.git/hooksOLD"
+	fi
+	echo "Backing up $1/.git/hooks to $1/.git/hooksOLD"
+	mv -f "$1/.git/hooks" "$1/.git/hooksOLD"
+fi
 ln -is "$HOOKS_DIRECTORY" "$1/.git/hooks"
 git config --global hooks.symlinksourcerepo "$HOOKS_DIRECTORY"
 
