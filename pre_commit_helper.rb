f2b6b00c4d36c5995618f3a939fe7333dd983a1a
Bob Gilmore <dev@bobgilmore.name>
2015-01-08 16:08:20 -0500

    Make param name more descriptive

--- pre_commit_helper.rb
+++ pre_commit_helper.rb
@@ -9,8 +9,8 @@ class PreCommitHelper
     return nil
   end
 
-  def self.deactivation_message(to_permanently_blank, key, value)
-    %{\nTo permanently #{to_permanently_blank} for this repo, run\ngit config hooks.#{key} #{value}\nand try again.\n\nTo permanently #{to_permanently_blank} it for *all* repos, run\ngit config --global hooks.#{key} #{value}\nand try again.\n--------------}
+  def self.deactivation_message(to_permanently_blank_for_repo, key, value)
+    %{\nTo permanently #{to_permanently_blank_for_repo} for this repo, run\ngit config hooks.#{key} #{value}\nand try again.\n\nTo permanently #{to_permanently_blank_for_repo} it for *all* repos, run\ngit config --global hooks.#{key} #{value}\nand try again.\n--------------}
   end
 
   def self.directory_excluded_from_checks?(directory)
