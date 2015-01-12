1f60bdbedd1970d5533d1aadf27d09d6c62672f7
Bob Gilmore <dev@bobgilmore.name>
2015-01-12 17:43:07 -0500

    Finish PreCommitHelper tests

--- spec/pre_commit_helper_spec.rb
+++ spec/pre_commit_helper_spec.rb
@@ -1,6 +1,7 @@
 load "pre_commit_helper.rb"
 
 RSpec.describe PreCommitHelper do
+
   describe ".directory_excluded_from_checks?" do
     it "should return false for a random directory" do
       expect(PreCommitHelper::directory_excluded_from_checks?("/home/foo/bar")).to be_falsey
@@ -14,4 +15,17 @@ RSpec.describe PreCommitHelper do
       expect(PreCommitHelper::directory_excluded_from_checks?("/my/dir/assets/foo")).to be_falsey
     end
   end
+
+  describe ".deactivation_message" do
+    it "should return a valid disabling message" do
+      expect(PreCommitHelper::deactivation_message("disable", "foo", "bar")).to include("To permanently disable for this repo, run\ngit config hooks.foo bar")
+    end
+  end
+
+  describe ".project_type" do
+    it "should return nil" do
+      expect(PreCommitHelper::project_type).to include("ruby")
+    end
+  end
+  
 end
