7f3339c078c08d2a4e4efceb0089a3701057196c
Bob Gilmore <dev@bobgilmore.name>
2015-01-13 09:40:40 -0500

    Make StrictParenSpacing return error messages, not print them

--- strict_paren_spacing_checker.rb
+++ strict_paren_spacing_checker.rb
@@ -3,10 +3,15 @@
 class StrictParenSpacingChecker
   SHELL_SCRIPT_EXTENSIONS = [ '.sh', '.bash', '' ]
 
-  OPEN_SMOOTH_SPACE  = /\([ \t]+/
-  SPACE_CLOSE_SMOOTH = /[ \t]+\)/
-  OPEN_SQUARE_SPACE  = /\[[ \t]+/
-  SPACE_CLOSE_SQUARE = /[ \t]+\]/
+  OPEN_SMOOTH_SPACE  = "( "
+  SPACE_CLOSE_SMOOTH = " )"
+  OPEN_SQUARE_SPACE  = "[ "
+  SPACE_CLOSE_SQUARE = " ]"
+
+  OPEN_SMOOTH_SPACE_REGEXP  = /\([ \t]+/
+  SPACE_CLOSE_SMOOTH_REGEXP = /[ \t]+\)/
+  OPEN_SQUARE_SPACE_REGEXP  = /\[[ \t]+/
+  SPACE_CLOSE_SQUARE_REGEXP = /[ \t]+\]/
 
   def self.necessary?
     val = `git config hooks.requirestrictparenspacing`.strip
@@ -14,28 +19,32 @@ class StrictParenSpacingChecker
   end
 
   def check(dir, file, changed_code_for_file)
+    messages = [ ]
+    deactivation_message = PreCommitHelper::deactivation_message("allow", "requirestrictparenspacing", false)
+    
     if !SHELL_SCRIPT_EXTENSIONS.include?(File.extname(file)) && !PreCommitHelper::directory_excluded_from_checks?(dir)
-      if changed_code_for_file.match(OPEN_SMOOTH_SPACE)
-        puts %{Warning: git pre-commit hook is suspicious of committing lines with "( " to #{file}\nThis may be OK, or not, depending on your project requirements.}
-        puts PreCommitHelper::deactivation_message("allow", "requirestrictparenspacing", false)
-        error_found = true
+      if changed_code_for_file.match(OPEN_SMOOTH_SPACE_REGEXP)
+        messages << warning_message(file, OPEN_SMOOTH_SPACE)
+        messages << deactivation_message
       end
-      if changed_code_for_file.match(SPACE_CLOSE_SMOOTH)
-        puts %{Warning: git pre-commit hook is suspicious of committing lines with " )" to #{file}\nThis may be OK, or not, depending on your project requirements.}
-        puts PreCommitHelper::deactivation_message("allow", "requirestrictparenspacing", false)
-        error_found = true
+      if changed_code_for_file.match(SPACE_CLOSE_SMOOTH_REGEXP)
+        messages << warning_message(file, SPACE_CLOSE_SMOOTH)
+        messages << deactivation_message
       end
-      if changed_code_for_file.match(OPEN_SQUARE_SPACE)
-        puts %{Warning: git pre-commit hook is suspicious of committing lines with "[ " to #{file}\nThis may be OK, or not, depending on your project requirements.}
-        puts PreCommitHelper::deactivation_message("allow", "requirestrictparenspacing", false)
-        error_found = true
+      if changed_code_for_file.match(OPEN_SQUARE_SPACE_REGEXP)
+        messages << warning_message(file, OPEN_SQUARE_SPACE)
+        messages << deactivation_message
       end
-      if changed_code_for_file.match(SPACE_CLOSE_SQUARE)
-        puts %{Warning: git pre-commit hook is suspicious of committing lines with " ]" to #{file}\nThis may be OK, or not, depending on your project requirements.}
-        puts PreCommitHelper::deactivation_message("allow", "requirestrictparenspacing", false)
-        error_found = true
+      if changed_code_for_file.match(SPACE_CLOSE_SQUARE_REGEXP)
+        messages << warning_message(file, SPACE_CLOSE_SQUARE)
+        messages << deactivation_message
       end
     end
+    messages
+  end
+
+  def warning_message(file, bad_expression)
+    %{Warning: git pre-commit hook is suspicious of committing lines with "#{bad_expression}" to #{file}\nThis may be OK, or not, depending on your project requirements.}
   end
 
 end
