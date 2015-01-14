2256560fb8975b53232bf86e6807ad71a3a742ff
Bob Gilmore <dev@bobgilmore.name>
2015-01-13 20:24:49 -0500

    Turn the Helper into a Module

--- pre_commit_helper.rb
+++ pre_commit_helper.rb
@@ -1,6 +1,6 @@
-class PreCommitHelper
+module PreCommitHelper
 
-  def self.project_type
+  def project_type
     toplevel = `git rev-parse --show-toplevel`.strip
     gemfile = File.join(toplevel, 'Gemfile')
     return 'ruby' if File.exist?(gemfile)
@@ -9,15 +9,15 @@ class PreCommitHelper
     return nil
   end
 
-  def self.deactivation_message(to_permanently_blank_for_repo, key, value)
+  def deactivation_message(to_permanently_blank_for_repo, key, value)
     %{\nTo permanently #{to_permanently_blank_for_repo} for this repo, run\ngit config hooks.#{key} #{value}\nand try again.\n\nTo permanently #{to_permanently_blank_for_repo} it for *all* repos, run\ngit config --global hooks.#{key} #{value}\nand try again.\n--------------}
   end
 
-  def self.directory_excluded_from_checks?(directory)
+  def directory_excluded_from_checks?(directory)
     !!(/assets\// =~ directory && /\/vendor/ =~ directory)
   end
 
-  def self.git_config_val_for_hook(hook_name)
+  def git_config_val_for_hook(hook_name)
     `git config hooks.#{hook_name}`.strip
   end
 
