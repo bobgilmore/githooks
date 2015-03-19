f249d3817e05ae372525c20586180a5ee06293fe
Bob Gilmore <dev@bobgilmore.name>
2015-03-19 00:32:11 -0700

    Initial impl of simple_deactivatable_rejection_checker

---
+++ pre_commit/simple_deactivatable_rejection_checker.rb
@@ -0,0 +1,44 @@
+class SimpleDeactivatableRejectionChecker
+  attr_reader :messages
+
+  def deactivation_message
+    PreCommitHelper.deactivation_message("allow", @hook_key, false)
+  end
+
+  def initialize(opts)
+    @dir = opts[:dir]
+    @file = opts[:file]
+    @changed_code = opts[:changes]
+    @hook_key = opts[:hook_key]
+    @regexp = opts[:regexp]
+    @warning_message = opts[:warning_message]
+
+    @pref_on = !!opts[:pref_on]
+    @messages = examine_code
+  end
+
+  def errors?
+    !@messages.empty?
+  end
+
+  def examine_code
+    return [] unless use_for_project?
+    mess = []
+    if !PreCommitHelper.directory_excluded_from_checks?(@dir)
+      mess << warning_message if @changed_code.match(@regexp)
+    end
+    mess
+  end
+
+  private
+
+  def use_for_project?
+    val = PreCommitHelper.git_config_val_for_hook(@hook_key)
+    val.empty? || (val == 'true') || @pref_on
+  end
+
+  def warning_message
+    %{#{@warning_message} in #{@file}}
+  end
+
+end
