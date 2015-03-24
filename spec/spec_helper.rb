329b883c7f7b0c615d29164586fcc0823088b58f
Bob Gilmore <dev@bobgilmore.name>
2015-03-24 16:22:49 -0400

    Require use of expect rather than should

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $:.unshift "#{File.dirname(__FILE__)}/.."
 $:.unshift "#{File.dirname(__FILE__)}/../pre_commit"
+
+require "rspec"
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
