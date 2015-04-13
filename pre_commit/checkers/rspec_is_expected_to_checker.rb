f128c2d2c164703abf0b2fa335aa4f8dac6212cd
Bob Gilmore <dev@bobgilmore.name>
2015-04-13 17:04:14 -0400

    Fix is_expected.to regex

--- pre_commit/checkers/rspec_is_expected_to_checker.rb
+++ pre_commit/checkers/rspec_is_expected_to_checker.rb
@@ -5,7 +5,7 @@ class RspecIsExpectedToChecker < SimpleDeactivatableRegexpChecker
   def initialize(opts)
     merge_in = {
       hook_key: "forbid-rspec-is-expected-to",
-      regexp: /is_expected.to/,
+      regexp: /is_expected\.to/,
       warning_message: %{RSpec 2.x "should" is preferred to RSpec 3.x "is_expected.to" in #{opts[:file]}}
     }
     super(opts.merge(merge_in))
