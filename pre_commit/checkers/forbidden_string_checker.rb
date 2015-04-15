c184ddb900aca603a2773aee94e8cd7075c00835
Bob Gilmore <dev@bobgilmore.name>
2015-04-15 10:52:49 -0400

    Fix bug in Node project detection

--- pre_commit/checkers/forbidden_string_checker.rb
+++ pre_commit/checkers/forbidden_string_checker.rb
@@ -18,7 +18,7 @@ class ForbiddenStringChecker
     FORBIDDEN_STRINGS.each do |re|
       mess << %{"#{$1 || $&}" in #{@file}} if @changed_code.match(re)
     end
-    if @project_type == :node
+    if @project_type != :node
       FORBIDDEN_STRINGS_EXCEPT_IN_NODE.each do |re2|
         if @changed_code.match(re2)
           mess << %{"#{$1 || $&}" in #{@file} outside of Node.js projects.\nIf this *is* a Node project, run\n\nnpm init\n\nat the top level of the project to add a package.json file. See\nhttps://devcenter.heroku.com/articles/getting-started-with-nodejs#declare-dependencies-with-npm\nfor more information.}
