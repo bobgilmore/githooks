c9495047fe1dd148f856e6617a760e2fca258be0
Bob Gilmore <dev@bobgilmore.name>
2017-03-08 22:21:07 -0500

    Clean up context messages for private key specs

--- spec/pre_commit/checkers/private_key_checker_spec.rb
+++ spec/pre_commit/checkers/private_key_checker_spec.rb
@@ -6,12 +6,12 @@ load "pre_commit/checkers/private_key_checker.rb"
 RSpec.describe PrivateKeyChecker do
   let(:checker_class) { PrivateKeyChecker }
 
-  context "code with private keys" do
+  context "code with no private keys" do
     subject(:checker) { test_class_with_change(checker_class, "Hello") }
     it_should_behave_like "it finds no error"
   end
 
-  context "code with an a private key indicator" do
+  context "code with a private key indicator" do
     subject(:checker) { test_class_with_change(checker_class, "foo; PRIVATE KEY is there!") }
     it_should_behave_like "it finds an error"
   end
